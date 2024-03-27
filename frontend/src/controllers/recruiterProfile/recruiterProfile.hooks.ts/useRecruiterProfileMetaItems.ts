ad435e7fccec608070dbbca531e9f8c45475ce57
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2024-03-27 14:03:54 +0200

    added a null option to the city column, removed an unnecessary picture, minor changes in the logic of displaying icons in MetaItems, package files returned to their original state

--- frontend/src/controllers/recruiterProfile/recruiterProfile.hooks.ts/useRecruiterProfileMetaItems.ts
+++ frontend/src/controllers/recruiterProfile/recruiterProfile.hooks.ts/useRecruiterProfileMetaItems.ts
@@ -2,10 +2,13 @@ import { useMemo } from 'react';
 import { RecruiterProfile } from '@/controllers/graphql/generated';
 import { getFilledValue } from '@/lib/getFilledValue';
 import { RecruiterProfileMetaItems } from '../recruiterProfile.typedefs';
+import { IconLocation } from '@/ui/icons/general/IconLocation';
+import { FCIcon } from '@/ui/icons/typedefs';
 
 export interface MetaItem {
   name: RecruiterProfileMetaItems,
   text: string,
+  icon?: FCIcon,
 }
 
 interface UseRecruiterProfileMetaItems {
@@ -35,6 +38,7 @@ export const useRecruiterProfileMetaItems: UseRecruiterProfileMetaItems = (
       metaItems.push({
         name: RecruiterProfileMetaItems.City,
         text: getFilledValue(profile.city),
+        icon: IconLocation,
       });
     }
 
