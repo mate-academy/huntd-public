3fc315eb6f622350391ad9645ca0700218058c97
root <root@ZenBook>
2024-08-30 23:39:49 +0300

    added error handling to migrations and seeder files

--- api/migrations/20240829204725-add-city-to-recruiter-profiles.js
+++ api/migrations/20240829204725-add-city-to-recruiter-profiles.js
@@ -1,12 +1,20 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('recruiter_profiles', 'city', {
-      type: Sequelize.STRING(255),
-      allowNull: true,
-    });
+    try {
+      await queryInterface.addColumn('recruiter_profiles', 'city', {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      });
+    } catch (error) {
+      throw new Error(`Error during recruiter_profiles migration UP: ${error}`);
+    }
   },
 
   down: async (queryInterface) => {
-    await queryInterface.removeColumn('recruiter_profiles', 'city');
+    try {
+      await queryInterface.removeColumn('recruiter_profiles', 'city');
+    } catch (error) {
+      throw new Error(`Error during recruiter_profiles migration DOWN: ${error}`);
+    }
   },
 };
--- api/seeders/20240830181951-add-city-to-recruiter-profile.js
+++ api/seeders/20240830181951-add-city-to-recruiter-profile.js
@@ -3,33 +3,41 @@ const CITY_TO_SET = 'Kyiv';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.query(`
-      UPDATE "recruiter_profiles"
-      SET city = :city
-      WHERE "user_id" IN (
-        SELECT id FROM "users" WHERE email = :email
-      )`,
-    {
-      replacements: {
-        email: EMAIL_TO_UPDATE,
-        city: CITY_TO_SET,
-      },
-      type: Sequelize.QueryTypes.UPDATE,
-    });
+    try {
+      await queryInterface.sequelize.query(`
+        UPDATE "recruiter_profiles"
+        SET city = :city
+        WHERE "user_id" IN (
+          SELECT id FROM "users" WHERE email = :email
+        )`,
+      {
+        replacements: {
+          email: EMAIL_TO_UPDATE,
+          city: CITY_TO_SET,
+        },
+        type: Sequelize.QueryTypes.UPDATE,
+      });
+    } catch (error) {
+      throw new Error(`Error during recruiter_profiles seeding UP: ${error}`);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.query(`
-      UPDATE "recruiter_profiles"
-      SET city = NULL
-      WHERE "user_id" IN (
-        SELECT id FROM "users" WHERE email = :email
-      )`,
-    {
-      replacements: {
-        email: EMAIL_TO_UPDATE,
-      },
-      type: Sequelize.QueryTypes.UPDATE,
-    });
+    try {
+      await queryInterface.sequelize.query(`
+        UPDATE "recruiter_profiles"
+        SET city = NULL
+        WHERE "user_id" IN (
+          SELECT id FROM "users" WHERE email = :email
+        )`,
+      {
+        replacements: {
+          email: EMAIL_TO_UPDATE,
+        },
+        type: Sequelize.QueryTypes.UPDATE,
+      });
+    } catch (error) {
+      throw new Error(`Error during recruiter_profiles seeding DOWN: ${error}`);
+    }
   },
 };
