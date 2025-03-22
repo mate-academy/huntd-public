a068780c6f3728f66b048d8fdb0d4c64deb22eb9
Oleksii Yurchenko <oleksii.yurchenko@techmagic.co>
2025-03-23 01:10:15 +0200

    feat(huntd-test) added mobile support

--- frontend/src/components/Profile/ProfilePreview/ProfileMeta/RecruiterProfileMeta.tsx
+++ frontend/src/components/Profile/ProfilePreview/ProfileMeta/RecruiterProfileMeta.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import cn from 'classnames';
 import { MetaItem } from '@/controllers/recruiterProfile/recruiterProfile.hooks.ts/useRecruiterProfileMetaItems';
 import typography from '@/ui/typography/typography.module.scss';
-import { RecruiterProfileMetaItems } from '@/controllers/recruiterProfile/recruiterProfile.typedefs';
-import { IconLocation } from '@/ui/icons/general/IconLocation';
 import styles from './ProfileMeta.module.scss';
 
 interface Props {
-  items: MetaItem[]
+  items: MetaItem[];
 }
 export const RecruiterProfileMeta = React.memo<Props>((props) => {
   const { items } = props;
@@ -19,7 +17,7 @@ export const RecruiterProfileMeta = React.memo<Props>((props) => {
           className={cn(styles.recruiterMetaItem, typography.smallHeading)}
           key={item.name}
         >
-          {item.name === RecruiterProfileMetaItems.City && <IconLocation />}
+          {item.icon && <item.icon />}
           {item.text}
         </li>
       ))}
