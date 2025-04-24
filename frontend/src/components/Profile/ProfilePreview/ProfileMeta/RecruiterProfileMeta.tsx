1e67908250a30b8a1a7c1c311fc03634b60d31e4
Anton Poliaiev <wymsa.email@gmail.com>
2025-04-24 18:54:17 +0300

    feat: completed tasks

--- frontend/src/components/Profile/ProfilePreview/ProfileMeta/RecruiterProfileMeta.tsx
+++ frontend/src/components/Profile/ProfilePreview/ProfileMeta/RecruiterProfileMeta.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import cn from 'classnames';
 import { MetaItem } from '@/controllers/recruiterProfile/recruiterProfile.hooks.ts/useRecruiterProfileMetaItems';
 import typography from '@/ui/typography/typography.module.scss';
+import { RecruiterProfileMetaItems } from '@/controllers/recruiterProfile/recruiterProfile.typedefs';
+import { IconLocation } from '@/ui/icons/general/IconLocation';
 import styles from './ProfileMeta.module.scss';
 
 interface Props {
@@ -17,7 +19,12 @@ export const RecruiterProfileMeta = React.memo<Props>((props) => {
           className={cn(styles.recruiterMetaItem, typography.smallHeading)}
           key={item.name}
         >
-          {item.text}
+          <>
+            {item.name === RecruiterProfileMetaItems.City ? (
+              <IconLocation className={styles.locationIcon} />
+            ) : null}
+            {item.text}
+          </>
         </li>
       ))}
     </ul>
